b088a4e4d7c0ffc3f5235504745aaff380f6785d
Artur <artur17harutyunyan@gmail.com>
2023-07-13 20:55:05 +0400

    added comments

--- graph.js
+++ graph.js
@@ -3,12 +3,14 @@ class Graph {
     this.adjacencyList = {};
   }
 
+  //Adding vertex
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) {
       this.adjacencyList[vertex] = new Set();
     }
   }
 
+  //adding edges from vertex to vertex
   addEdge(firstVertex, secondVertex) {
     if (!this.adjacencyList[firstVertex]) {
       this.addVertex(firstVertex);
@@ -22,6 +24,7 @@ class Graph {
     this.adjacencyList[secondVertex].add(firstVertex);
   }
 
+  //checking if vertexes have edges
   hasEdge(firstVertex, secondVertex) {
     return (
       this.adjacencyList[firstVertex].has(secondVertex) &&
@@ -29,6 +32,7 @@ class Graph {
     );
   }
 
+  //removing edges
   removeEdge(firstVertex, secondVertex) {
     const existEdge =
       this.adjacencyList[firstVertex].has(secondVertex) &&
@@ -40,11 +44,13 @@ class Graph {
     }
   }
 
+  //removing vertex
   removeVertex(vertex) {
     if (!this.adjacencyList[vertex]) {
       return 'The vertex does not exist';
     }
 
+    // removing edges for current vertex
     for (const vertexItem of this.adjacencyList[vertex]) {
       this.removeEdge(vertex, vertexItem);
     }
@@ -52,6 +58,7 @@ class Graph {
     delete this.adjacencyList[vertex];
   }
 
+  //logging adjacency list
   logGraph() {
     for (const vertex in this.adjacencyList) {
       console.log(`${vertex} -> ${[...this.adjacencyList[vertex]]}`);
